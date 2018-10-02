320a75ddb57fbff55d9ad17c7f6330991e41ff9a
Tristan Forward <tforward@users.noreply.github.com>
2018-10-01 20:07:59 -0400

    First Commit

---
+++ src/scripts/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const myApp = Object.create(null);
+
+// ======================================================================
+// App
+// ======================================================================
+
+myApp.main = function main() {
+};
+
+myApp.initApplication = function init() {
+  myApp.main();
+};
+
+// Handler when the DOM is fully loaded
+document.onreadystatechange = function onreadystatechange() {
+  if (document.readyState === "complete") {
+    myApp.initApplication(document.readyState);
+  } else {
+    // Do something during loading [opitional]
+  }
+};
+
+// ======================================================================
